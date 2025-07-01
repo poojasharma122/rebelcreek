b8bc7c87533861fdcac00e21634d5e1a9d73ad63
poojasharma122 <design.avology@gmail.com>
2025-07-01 12:33:11 +0530

    changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -67,3 +67,28 @@ $('.home_featured_slider').slick({
 });
 // Slick Slider JS End
 
+// FAQ JS Start
+let question = document.querySelectorAll(".question");
+
+question.forEach(question => {
+  question.addEventListener("click", event => {
+    const active = document.querySelector(".question.active");
+    if(active && active !== question ) {
+      active.classList.toggle("active");
+      active.nextElementSibling.style.maxHeight = 0;
+    }
+    question.classList.toggle("active");
+    const answer = question.nextElementSibling;
+    if(question.classList.contains("active")){
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } else {
+      answer.style.maxHeight = 0;
+    }
+  })
+})
+
+
+document.querySelector(".form-wrapper").addEventListener("submit", function(e) {
+  e.preventDefault(); 
+});
+// FAQ JS End
