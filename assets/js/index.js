103f5fa7c61f63995ca9604edee6d66a7f2f2570
poojasharma122 <design.avology@gmail.com>
2025-07-01 16:29:37 +0530

    cahnge

--- assets/js/index.js
+++ assets/js/index.js
@@ -87,8 +87,29 @@ question.forEach(question => {
   })
 })
 
-
-document.querySelector(".form-wrapper").addEventListener("submit", function(e) {
-  e.preventDefault(); 
-});
 // FAQ JS End
+
+let form = document.querySelector(".form-wrapper");
+
+if (form) {
+  form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+  });
+}
+
+// Read More JS Start
+$('.subpage-readmore-btn').click(function (e) {
+  e.preventDefault();
+  const $button = $(this);
+  const $content = $button.closest('.destination-card-text').find('.destination-para');
+
+  $content.stop(true, true).slideToggle(500, function () {
+      const isVisible = $content.is(':visible');
+
+      $button.html(isVisible
+          ? 'Read Less <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>' // UP arrow
+          : 'Read More <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>' // DOWN arrow
+      );
+  });
+});
+// Read More JS end
